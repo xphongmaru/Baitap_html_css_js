ef75b24f0a7b9185178327a9d24d581518ae4fd7
xphongmaru <phamxuanphong333@gmail.com>
2023-11-11 16:16:24 +0700

    bai tap html

---
+++ main.js
@@ -0,0 +1,36 @@
+//alert("Hi Javasript");
+
+var fullName = "Pham Xuan Phong";
+var age=19;
+
+// alert(fullName);
+// alert(age);
+
+// console.log('day la 1 dong '+ fullName+'\t'+age);
+// console.warn(typeof(fullName));
+// confirm(19+age); //xac nhan
+// prompt('Nhap vao gi day');
+
+// setTimeout(function() {
+//     alert('Thong bao la: ');//chay ham trong mot khoang thoi gian 
+// },1000);
+
+// setInterval(function() {
+//     console.log('Thong bao la: ');//chay ham lien tuc lap di lap lai
+// },1000);
+
+var kq = true;
+console.log(!kq);
+
+
+/**
+ * <p></p> dinh nghia mot doan van ban
+ * <br> xuong dong trong doan van ban
+ * align="left/right/center"; can le
+ * <hr></hr> :tao duong ke - size: px ;color: "mau or ma mau" ;width: % do day; align: can vi tri
+ * <ul> <li></li> </ul>: tao danh sach 
+ * <a> tao lien ket href = link; target = quy dinh cach mo trang; title = di chuot vao se hien mo ta
+ * tao anh
+ * <img src="Pictures\phong.jpg" alt="Học lập trình qua dự án cùng chuyên gia" width="213.5px" height="320px">
+
+ */
